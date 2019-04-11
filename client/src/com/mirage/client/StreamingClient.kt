acc98b25f28f6e65d76871360321d29de7ae59a2
Mirage-A <zaitsev.eo@phystech.edu>
2019-04-11 19:32:05 +0300

    added custom lambdas for io events

--- client/src/com/mirage/client/StreamingClient.kt
+++ client/src/com/mirage/client/StreamingClient.kt
@@ -2,37 +2,60 @@
 package com.mirage.client
 
 import io.netty.buffer.ByteBuf
+import io.reactivex.netty.channel.Connection
 import io.reactivex.netty.protocol.tcp.client.TcpClient
 import io.reactivex.netty.util.StringLineDecoder
+import rx.schedulers.Schedulers
 import rx.subjects.PublishSubject
 
+class Client (
+    private val onConnectionEstablished: (Connection<String, ByteBuf>) -> Unit = {println("Connection established")},
+    private val onNewServerMessage: (String) -> Unit = {n -> println(n)},
+    private val onError: (Connection<String, ByteBuf>, Throwable) -> Unit = {c, _ -> c.close()}
+){
+
+    private val out = PublishSubject.create<String>()
+
+    private val client = TcpClient.newClient("localhost", 55555)
+
+    fun start() {
+        client.addChannelHandlerLast<ByteBuf, String>("string-decoder") { StringLineDecoder() }
+            .createConnectionRequest()
+            .flatMap {connection ->
+                onConnectionEstablished(connection)
+                connection.writeStringAndFlushOnEach(out).subscribe(
+                    {},
+                    {onError(connection, it)},
+                    {connection.close()}
+                )
+                connection.input
+            }
+            .subscribe(
+                {onNewServerMessage(it)},
+                {},
+                {}
+            )
+    }
+
+    /**
+     * msg should end with '\n'
+     */
+    fun sendMessage(msg : String) {
+        out.onNext(msg)
+    }
+
+}
+
 fun main() {
-    val out = PublishSubject.create<String>()
+
+    val c = Client()
+
+    c.start()
 
     val t = Timer(2000) {
-        out.onNext(it.toString() + "\n")
+        c.sendMessage(it.toString() + "\n")
     }
+
     t.start()
 
-    TcpClient.newClient("localhost", 55555)
-        .addChannelHandlerLast<ByteBuf, String>("string-decoder") { StringLineDecoder() }
-        .createConnectionRequest()
-        .flatMap {connection ->
-            println("Connection established")
-            connection.writeStringAndFlushOnEach(out).subscribe(
-                {},
-                {connection.close()},
-                {connection.close()}
-            )
-            connection.input
-        }
-        .subscribe(
-            ::println,
-            {println("ERROR")},
-            {println("COMPLETED")}
-        )
-        //.forEach(::println)
-
-    println("Client initialization finished")
-    Thread.sleep(10000)
 }
--- server/src/com/mirage/server/StreamingServer.kt
+++ server/src/com/mirage/server/StreamingServer.kt
@@ -4,36 +4,62 @@ package com.mirage.server
 import io.netty.buffer.ByteBuf
 import io.netty.channel.*
 import io.netty.channel.socket.DuplexChannel
+import io.reactivex.netty.channel.Connection
 import io.reactivex.netty.protocol.tcp.server.TcpServer
 import io.reactivex.netty.util.StringLineDecoder
 import rx.subjects.PublishSubject
 import java.net.SocketAddress
 
 
+class Server(
+    private val onNewConnection: (Connection<ByteBuf, ByteBuf>) -> Unit = {println("new connection!")},
+    private val onNewClientMessage: (Connection<ByteBuf, ByteBuf>, String) -> Unit = {_, n -> println(n)},
+    private val onError: (Connection<ByteBuf, ByteBuf>, Throwable) -> Unit = {c, _ -> c.close()}
+) {
+
+    private val server: TcpServer<ByteBuf, ByteBuf> = TcpServer.newServer(55555)
+
+    private val out = PublishSubject.create<String>()
+
+    /**
+     * msg should end with '\n'
+     */
+    fun sendMessage(msg: String) {
+        out.onNext(msg)
+    }
+
+    fun start() {
+        server.start {connection ->
+                onNewConnection(connection)
+                connection
+                    .addChannelHandlerFirst<String, ByteBuf>("string-decoder", StringLineDecoder())
+                    .input
+                    .subscribe(
+                        {onNewClientMessage(connection, it)},
+                        {onError(connection, it)},
+                        {println("onCompleted"); connection.close()}
+                    )
+                connection.writeStringAndFlushOnEach(out)
+            }
+    }
+
+    fun join() {
+        server.awaitShutdown()
+    }
+
+}
 
 fun main() {
-    val server: TcpServer<ByteBuf, ByteBuf>
 
-    val out = PublishSubject.create<String>()
+    val s = Server()
+
+    s.start()
 
     val t = Timer(1500) {
-        out.onNext(it.toString() + "\n")
+        s.sendMessage(it.toString() + "\n")
     }
     t.start()
 
-    server = TcpServer.newServer(55555)
-        .start {connection ->
-            println("new connection!")
-            connection
-                .addChannelHandlerFirst<String, ByteBuf>("string-decoder", StringLineDecoder())
-                .input
-                .subscribe(
-                    {println("onNext $it")},
-                    {println("onError"); connection.close()},
-                    {println("onCompleted"); connection.close()}
-                )
-            connection.writeStringAndFlushOnEach(out)
-        }
+    s.join()
 
-    server.awaitShutdown()
 }
